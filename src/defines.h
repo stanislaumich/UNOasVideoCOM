e1377a9ba3988dcd56896650ec8fdf6754d2befb
Stas <staslz42stas@gmail.com>
2019-09-18 10:33:50 +0300

    Промежуточно вычистил определения

--- src/defines.h
+++ src/defines.h
@@ -1,4 +1,4 @@
-#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CS A3
 #define LCD_CD A2 // Command/Data goes to Analog 2
 #define LCD_WR A1 // LCD Write goes to Analog 1
 #define LCD_RD A0 // LCD Read goes to Analog 0
