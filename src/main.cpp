78bd970fb1a05f65ac5a5d1365c2628c567b66b2
Stas <staslz42stas@gmail.com>
2019-09-18 09:30:26 +0300

    Печатает русским только в основном шрифте

--- src/main.cpp
+++ src/main.cpp
@@ -5,74 +5,95 @@
 #include "defines.h"
 //#include <Fonts/FreeSans18pt7b.h>
 //#include <Fonts/FreeMono12pt7b.h>
-#include <FreeDefaultFonts.h>
+//#include <FreeDefaultFonts.h>
 //#include <Fonts/FreeMonoBold12pt7b.h>
-#include <FontsRus/TimesNRCyr10.h> 
+//#include <FontsRus/bahamas18.h> 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 MCUFRIEND_kbv tft;
 String inString="";
-
-/*
-int i=0;
-int tw;
-int th;
-int prevx=0;
-int prevy=0;
-int x=0;
-int y=0;
-int step=1;
-int speed = 0;
-char cstr[80];
-
-String g="";
-*/
-
+String pr="";
+int px;
+int py;
+int ps;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 String getValue(String data, char separator, int index)
-{
-int found = 0;
-int strIndex[] = {0, -1};
-int maxIndex = data.length()-1;
-
-for(int i=0; i<=maxIndex && found<=index; i++){
-if(data.charAt(i)==separator || i==maxIndex){
-found++;
-strIndex[0] = strIndex[1]+1;
-strIndex[1] = (i == maxIndex) ? i+1 : i;
-}
-}
-return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
-} 
-
+ {
+   int found = 0;
+   int strIndex[] = {0, -1};
+   int maxIndex = data.length()-1;
+   for(int i=0; i<=maxIndex && found<=index; i++){
+   if(data.charAt(i)==separator || i==maxIndex){
+   found++;
+   strIndex[0] = strIndex[1]+1;
+   strIndex[1] = (i == maxIndex) ? i+1 : i;
+   }
+  }
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+ } 
 
 
-void bar(String b){
+String utf8rus(String source)
+ {
+  int i,k;
+  String target;
+  unsigned char n;
+  char m[2] = { '0', '\0' };
+
+  k = source.length(); i = 0;
+
+  while (i < k) {
+    n = source[i]; i++;
+
+    if (n >= 0xC0) {
+      switch (n) {
+        case 0xD0: {
+          n = source[i]; i++;
+          if (n == 0x81) { n = 0xA8; break; }
+          if (n >= 0x90 && n <= 0xBF) n = n + 0x30;
+          break;
+        }
+        case 0xD1: {
+          n = source[i]; i++;
+          if (n == 0x91) { n = 0xB8; break; }
+          if (n >= 0x80 && n <= 0x8F) n = n + 0x70;
+          break;
+        }
+      }
+    }
+    m[0] = n; target = target + String(m);
+  }
+  return target;
+ }
+
+void bar(String b) 
+ {
         String part01 = getValue(b,' ',1);
         String part02 = getValue(b,' ',2);
         String part03 = getValue(b,' ',3);
         String part04 = getValue(b,' ',4);
         String part05 = getValue(b,' ',5);
         tft.fillRect(part01.toInt(),part02.toInt(),part03.toInt(),part04.toInt(),part05.toInt());
-}
-void clrscr(void){
- tft.fillScreen(BLACK);
- tft.drawFastHLine(0,0,tft.width()-1,GREEN) ;// ok
- tft.drawFastHLine(0,tft.height()-1,tft.width()-1,GREEN) ;// ok
- tft.drawFastVLine(0,0,tft.height()-1,GREEN) ;// ok
- tft.drawFastVLine(tft.width()-1,0,tft.width()-1,GREEN) ;// ok
-}
-String pr="";
-int px;
-int py;
-int ps;
-void text(String b){
-/////// стираем старое черным цветом
-  tft.setFont(&TimesNRCyr10pt8b);
+ }
+void clrscr(void)
+ {
+  tft.fillScreen(BLACK);
+  tft.drawFastHLine(0,0,tft.width()-1,GREEN) ;// ok
+  tft.drawFastHLine(0,tft.height()-1,tft.width()-1,GREEN) ;// ok
+  tft.drawFastVLine(0,0,tft.height()-1,GREEN) ;// ok
+  tft.drawFastVLine(tft.width()-1,0,tft.width()-1,GREEN) ;// ok
+ }
+
+
+
+void text(String b)
+ {
+   /////// стираем старое черным цветом
+  tft.setFont();//&Bahamas18pt8b);
   tft.setCursor(px, py);
   tft.setTextColor(BLACK);
   tft.setTextSize(ps);
-  tft.print(pr);
-/////// запоминаем что стирать и рисуем новое
+  tft.print(utf8rus(pr));
+   /////// запоминаем что стирать и рисуем новое
   String part01 = getValue(b,' ',1);
   String part02 = getValue(b,' ',2);
   String part03 = getValue(b,' ',3);
@@ -81,28 +102,27 @@ void text(String b){
   px = part01.toInt();
   py = part02.toInt();
   ps = part04.toInt();
-  tft.setFont(&TimesNRCyr10pt8b);
+  tft.setFont();//&Bahamas18pt8b);
   tft.setCursor(px, py);
   tft.setTextColor(part03.toInt());
   tft.setTextSize(ps);//;
-  tft.print(part05);
+  tft.print(utf8rus(part05));
   pr = part05;
-}
+ }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-void setup() {
+void setup()
+ {
   uartBegin();
   uartPrintln("+ Started Video"); 
   uint16_t ID = tft.readID(); uartPrintln(ID, HEX);
   tft.begin(ID); 
-  //tft.cp437(true);
+  tft.cp437(true);
   tft.setRotation(LANDSCAPE);
-  clrscr();
- 
-}
+  clrscr(); 
+ }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void loop() {
-  
+void loop() 
+ {
   while (uartAvailable() > 0) {
     char inChar = uartRead();
     inString += inChar;
@@ -129,4 +149,4 @@ void loop() {
         ;
     }      
   }
-}
+ }
